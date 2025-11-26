da51430acb524ba1b90e05e696008860896ed4b9
LucasVe95 <lucasveysset1@gmail.com>
2025-11-26 16:19:31 +0100

    Changement des nom des variable pour plus de clareté

--- plateau.py
+++ plateau.py
@@ -1,18 +1,21 @@
 class Battaille_Navale:
+    """
+    Crée une grille vide et affiche les coordonnées des cellules en temps réel.en collone et en ligne.
+    """
     def __init__(self):
         self.grid_size = 10
-        self.plateau = [['O' for _ in range(self.grid_size)] for i in range(self.grid_size)]
+        self.plateau = [['O' for j in range(self.grid_size)] for i in range(self.grid_size)]
     def __str__(self):
         """
         Afficher une grille vide avec des cellules identifiables (A1, B2, etc.). pour chaque joueur en affichant la grille du joueur actuel, puis l'autre quand c'est son tour
-        L'etat de la grille doit être mis à jour après chaque attaque pour refléter les touches et les ratés. puis remis a zero a la fin de la partie.
+        L'etat de la grille doit être mis à jour après chaque attaque pour refléter les touches et les ratés. puis remis a zero a la fin de la partie.En ligne et en collone.
         """
-        display = "  " + " ".join(str(i + 1).rjust(2) for i in range(self.grid_size)) + "\n"
+        affichage = "  " + " ".join([str(i + 1).rjust(2) for i in range(self.grid_size)]) + "\n"
         for i in range(self.grid_size):
-            row_label = chr(ord('A') + i)
-            display += row_label + " " + " ".join(self.plateau[i][j].rjust(2) for j in range(self.grid_size)) + "\n"
-        return display
+            ligne = chr(ord('A') + i) + " " + " ".join(self.plateau[i][j].rjust(2) for j in range(self.grid_size))
+            affichage += ligne + "\n"
+        return affichage
 #TEST UNITAIRE
 if __name__ == "__main__":
-    bataille_navale = Battaille_Navale()
-    print(bataille_navale)
+    plateau = Battaille_Navale()
+    print(plateau)
